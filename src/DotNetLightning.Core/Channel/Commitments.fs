6052a4635b39996ba65d53d6adaf8f1f9a31c721
Andrew Cann <shum@canndrew.org>
2020-12-07 16:34:07 +0800

    Remove WaitingForRevocation.Sent field
    This field was unused.

--- src/DotNetLightning.Core/Channel/Commitments.fs
+++ src/DotNetLightning.Core/Channel/Commitments.fs
@@ -79,7 +79,6 @@ type RemoteCommit = {
 
 type WaitingForRevocation = {
     NextRemoteCommit: RemoteCommit
-    Sent: CommitmentSignedMsg
     SentAfterLocalCommitmentIndex: CommitmentNumber
     ReSignASAP: bool
 }
