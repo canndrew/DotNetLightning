f95e3f304d7ba9467b3629e887a18ce492cc45a7
Andrew Cann <shum@canndrew.org>
2021-03-19 19:41:25 +0800

    WIP - test for tryGetFundsFromLocalCommitmentTx

--- tests/DotNetLightning.Core.Tests/TransactionTests.fs
+++ tests/DotNetLightning.Core.Tests/TransactionTests.fs
@@ -1,11 +1,14 @@
 module TransactionTests
 
+open System
 open ResultUtils
 
 open DotNetLightning.Transactions
 open DotNetLightning.Transactions.Transactions
 open DotNetLightning.Utils
 open DotNetLightning.Crypto
+open DotNetLightning.Channel
+open DotNetLightning.Serialization
 open Expecto
 open NBitcoin
 
@@ -13,6 +16,71 @@ let n = Network.RegTest
 
 [<Tests>]
 let testList = [
+    testCase "check tryGetFundsFromLocalCommitmentTx" <| fun _ ->
+        let rand = new Random()
+
+        let localNodeMasterPrivKey =
+            let extKey = new ExtKey()
+            NodeMasterPrivKey extKey
+        let localNodeSecret = localNodeMasterPrivKey.NodeSecret()
+        let localNodeId = localNodeSecret.NodeId()
+        let localChannelPrivKeys = localNodeMasterPrivKey.ChannelPrivKeys (rand.Next(1, 100))
+        let localChannelPubKeys = localChannelPrivKeys.ToChannelPubKeys()
+        let localDestPrivKey = new Key()
+        let localDestPubKey = localDestPrivKey.PubKey
+
+        let remoteNodeMasterPrivKey =
+            let extKey = new ExtKey()
+            NodeMasterPrivKey extKey
+        let remoteNodeSecret = remoteNodeMasterPrivKey.NodeSecret()
+        let remoteNodeId = remoteNodeSecret.NodeId()
+        let remoteChannelPrivKeys = remoteNodeMasterPrivKey.ChannelPrivKeys (rand.Next(1, 100))
+        let remoteChannelPubKeys = remoteChannelPrivKeys.ToChannelPubKeys()
+        let remoteDestPrivKey = new Key()
+        let remoteDestPubKey = remoteDestPrivKey.PubKey
+
+        let fundingScriptPubKey =
+            Scripts.funding
+                (state.InputInitFunder.ChannelPrivKeys.ToChannelPubKeys().FundingPubKey)
+                msg.FundingPubKey
+        let fundingDestination = fundingScriptPubKey.WitHash :> IDestination
+        let fundingAmount = state.InputInitFunder.FundingSatoshis
+        let fundingScriptCoin = ScriptCoin(Coin(randomOutpoint, 
+
+        let _commitments =
+            let localParams = {
+                NodeId = localNodeId
+                ChannelPubKeys = localChannelPubKeys
+                DustLimitSatoshis = 546L |> Money.Satoshis
+                MaxHTLCValueInFlightMSat = 10_000_000L |> LNMoney
+                ChannelReserveSatoshis = 1000L |> Money.Satoshis
+                HTLCMinimumMSat = 1000L |> LNMoney
+                ToSelfDelay = 144us |> BlockHeightOffset16
+                MaxAcceptedHTLCs = 1000us
+                IsFunder = true
+                DefaultFinalScriptPubKey = localDestPubKey.ScriptPubKey
+                Features = FeatureBits.Zero
+            }
+            let remoteParams = {
+                NodeId = remoteNodeId
+                DustLimitSatoshis = 546 |> Money.Satoshis
+                MaxHTLCValueInFlightMSat = 10_000_000L |> LNMoney
+                ChannelReserveSatoshis = 1000L |> Money.Satoshis
+                HTLCMinimumMSat = 1000L |> LNMoney
+                ToSelfDelay = 144us |> BlockHeightOffset16
+                MaxAcceptedHTLCs = 1000us
+                ChannelPubKeys = remoteChannelPubKeys
+                Features = FeatureBits.Zero
+                MinimumDepth = 6us |> BlockHeightOffset32
+            }
+            {
+                LocalParams = localParams
+                RemoteParams = remoteParams
+                ChannelFlags = 0
+                FundingScriptCoin = 
+            }
+        ()
+
     testCase "check pre-computed transaction weights" <| fun _ ->
         let localRevocationPriv = [| for _ in 0..31 -> 0xccuy |] |> fun b -> new Key(b)
         let localPaymentPriv = [| for _ in 0..31 -> 0xdduy |] |> fun b -> new Key(b)
